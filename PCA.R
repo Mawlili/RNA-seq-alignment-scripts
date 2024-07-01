4d8e6fcc4c00b81a9c2c7ac96513ad905b4f6616
William Wu <149866883+Mawlili@users.noreply.github.com>
2024-07-01 00:45:40 -0500

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -1,9 +1,20 @@
+#library
 library(DESeq2)
 library(tximeta)
+library(RUVSeq)
+library(MASS)
 
+#selecting house keeping gene
 gse <- summarizeToGene(se)
-expression_data <- assay(gse)
-expression_data_log <- log2(expression_data + 1)
+counts <- round(assay(gse))
+condition.GA <- coldata$condition.GA
+dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition.GA)
+dds <- DESeq(dds)
+res <- results(dds)
+housekeeping_genes <- rownames(res[which(res$padj > 0.05), ])
+set <- RUVg(expression_data, housekeeping_genes, k=1)
+#log normalization and pca 
+pca_data <- prcomp(t(assay(rlog_data)))
 pca_plot <- ggplot(coldata, aes(x = PC1, y = PC2, color =  condition.mother_highest_education)) +
   geom_point(size = 3) +
   xlab(paste0("PC1: ", round(100 * summary(pca)$importance[2, 1], 1), "% variance")) +
