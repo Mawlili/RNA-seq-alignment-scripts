402ab253aa402b842c3165ca9f98c0b06406bec9
William Wu <149866883+Mawlili@users.noreply.github.com>
2024-07-03 21:04:40 -0500

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -14,10 +14,14 @@ dds <- DESeq(dds)
 res <- results(dds)
 housekeeping_genes <- rownames(res[which(res$padj > 0.05), ])
 
-set <- RUVg(counts, housekeeping_genes, k=3)
+#remove variance
+set <- RUVg(counts, housekeeping_genes, k=1)
 normalized_counts <- set$normalizedCounts
+
+#vst normalization
 dds_normalized <- DESeqDataSetFromMatrix(countData = normalized_counts, colData = coldata, design = ~ condition.GA)
 vst_data <- vst(dds_normalized)
+
 #log normalization and pca 
 pca_data <- prcomp(t(assay(vst_data)))
 pca_components <- data.frame(PC1 = pca_data$x[,1], PC2 = pca_data$x[,2])
@@ -27,7 +31,15 @@ pca_plot <- ggplot(pca_plot_data, aes(x = PC1, y = PC2, color =  condition.sex))
   xlab(paste0("PC1: ", round(100 * summary(pca)$importance[2, 1], 1), "% variance")) +
   ylab(paste0("PC2: ", round(100 * summary(pca)$importance[2, 2], 1), "% variance")) +
   ggtitle("PCA Plot") +
- # xlim(-150, 150) +
- # ylim(-200, 250)
+  xlim(-150, 150) +
+  ylim(-50, 50)
   theme_minimal()
- ggsave("/rsrch5/home/canbio/whwu1/pca_plot_k3.png", plot = pca_plot, width = 10, height = 8, units = "in", dpi = 300) 
+ ggsave("/rsrch5/home/canbio/whwu1/pca_plot_k1_2.png", plot = pca_plot, width = 10, height = 8, units = "in", dpi = 300) 
+
+#DESeqDataSetFromMatrix w1+sex+...
+diff_exp_transcript <- DESeqDataSetFromMatrix(countData = normalized_counts, colData = coldata, design = ~ W_1 + condition.sex + condition.ethnicity + condition.GROUP + condition.GA)
+diff_exp_isoform <- DESeqDataSetFromMatrix(countData = se, colData = coldata, design = ~ W_1 + condition.sex + condition.ethnicity + condition.GROUP + condition.GA)
+
+
+#gse and se
+
