34d3f0c0fdacea4f09a15fa47a5793af4f0b31da
William Wu <149866883+Mawlili@users.noreply.github.com>
2024-07-12 12:39:07 -0500

    Update pfas_analysis.R

--- pfas_analysis.R
+++ pfas_analysis.R
@@ -11,8 +11,10 @@ pfas_w1_cord_J <- paste0("J", pfas_w1_cord$condition.ID)
 pfas_w1_mat_J <- paste0("J", pfas_w1_mat$condition.ID)
 normalized_counts_filtered_cord <- normalized_counts[, colnames(normalized_counts) %in% pfas_w1_cord_J ]
 normalized_counts_filtered_mat <- normalized_counts[, colnames(normalized_counts) %in% pfas_w1_mat_J ]
+
+counts_filtered_cord <- counts[, colnames(counts) %in% pfas_w1_cord_J ]
 #construct deseqdataset and perfor deseq analysis
-cord_pfas_diff <- DESeqDataSetFromMatrix(countData = normalized_counts_filtered_cord, colData = pfas_w1_cord, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + cord_PFBA)
+cord_pfas_diff <- DESeqDataSetFromMatrix(countData = counts_filtered_cord, colData = pfas_w1_cord, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + cord_PFBA)
 mat_pfas_diff <- DESeqDataSetFromMatrix(countData = normalized_counts_filtered_mat, colData = pfas_w1_mat, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + mat_PFBA)
 dds_cord_pfas <- DESeq(cord_pfas_diff)
 dds_mat_pfas <- DESeq(mat_pfas_diff)
