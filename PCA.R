9f25f639b7b5ececbf2d2921d7c55331e381f612
William Wu <149866883+Mawlili@users.noreply.github.com>
2024-07-15 22:58:45 -0500

    Update PCA.R

--- PCA.R
+++ PCA.R
@@ -16,11 +16,14 @@ counts <- round(assay(gse))
 condition.GA <- coldata$condition.GA
 dds <- DESeqDataSetFromMatrix(countData = counts, colData = coldata, design = ~ condition.GA)
 dds <- DESeq(dds)
-res <- results(dds)
-housekeeping_genes <- rownames(res[which(res$padj > 0.05), ])
+res <- counts(dds, normalized = TRUE)
+gene_variance <- apply(res, 1, var)
+low_variance_genes <- names(sort(gene_variance)[1:100])
+
+#housekeeping_genes <- rownames(res[which(res$padj > 0.05), ])
 
 #remove variance
-set <- RUVg(counts, housekeeping_genes, k=1)
+set <- RUVg(counts, low_variance_genes, k=1)
 normalized_counts <- set$normalizedCounts
 
 #vst normalization
@@ -36,10 +39,10 @@ pca_plot <- ggplot(pca_plot_data, aes(x = PC1, y = PC2, color =  condition.sex))
   xlab(paste0("PC1: ", round(100 * summary(pca)$importance[2, 1], 1), "% variance")) +
   ylab(paste0("PC2: ", round(100 * summary(pca)$importance[2, 2], 1), "% variance")) +
   ggtitle("PCA Plot") +
-  xlim(-150, 150) +
-  ylim(-50, 50)
+ xlim(-20, 25) +
+  ylim(-100, 100)
   theme_minimal()
- ggsave("/rsrch5/home/canbio/whwu1/pca_plot_k1_2.png", plot = pca_plot, width = 10, height = 8, units = "in", dpi = 300) 
+ ggsave("pca_var.png", plot = pca_plot, width = 10, height = 8, units = "in", dpi = 300) 
 
 #DESeqDataSetFromMatrix w1+sex+...
 W_1 <- set$W
@@ -50,26 +53,7 @@ dds_transcript <- DESeq(diff_exp_transcript)
 dds_isoform <- DESeq(diff_exp_isoform)
 res_transcript <- results(dds_transcript)
 res_isoform <- results(dds_isoform)
-volcano_plot <- EnhancedVolcano(res_isoform,
-                lab = rownames(res_isoform),              # Labels for points
-                x = 'log2FoldChange',             # x-axis: log2 fold-change
-                y = 'pvalue',                     # y-axis: p-value
-                xlim = c(-5, 5),                  # x-axis limits
-                ylim = c(0, -log10(min(res$pvalue, na.rm = TRUE))),  # y-axis limits
-                pCutoff = 0.05,                   # p-value cutoff for significance
-                FCcutoff = 1.0,                   # Fold-change cutoff for significance
-                pointSize = 2.0,                  # Size of points
-                labSize = 3.0,                    # Size of labels
-                title = 'Volcano plot',           # Title of the plot
-                subtitle = 'Differential expression results',  # Subtitle
-                caption = 'Log2 fold-change vs. p-value',      # Caption
-                legendPosition = 'right',         # Position of legend
-                legendLabSize = 14,               # Size of legend labels
-                legendIconSize = 4.0)    
 
-png("volcano_plot.png")
-print(volcano_plot)
-dev.off()
 
 #pfas analysis
 pfas <- read_excel("/rsrch5/home/epi/bhattacharya_lab/users/whwu1/pfas.xlsx")
@@ -79,8 +63,8 @@ pfas_w1 <- inner_join(coldata_w1, pfas, by = "condition.SubjectID")
 #pfba gene family for transcript
 
 normalized_counts_f <- dds[rowSums(counts(dds)) > 1, ] 
-cord_pfas_diff <- DESeqDataSetFromMatrix(countData = normalized_counts, colData = pfas_w1, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + cord_PFBA)
-mat_pfas_diff <- DESeqDataSetFromMatrix(countData = normalized_counts, colData = pfas_w1, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + mat_PFBA)
+cord_pfas_diff <- DESeqDataSetFromMatrix(countData = counts, colData = pfas_w1, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex)
+mat_pfas_diff <- DESeqDataSetFromMatrix(countData = counts, colData = pfas_w1, design = ~ W_1 + condition.sex + condition.mother_ethnicity + condition.GROUP + condition.GA + mat_PFBA)
 dds_cord_pfas <- DESeq(cord_pfas_diff)
 dds_mat_pfas <- DESeq(mat_pfas_diff)
 res_cord_pfas <- results(cord_pfas_diff)
