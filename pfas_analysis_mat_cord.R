df5259c87c61259bbef9810b014dcf8e33b7cc0b
William Wu <149866883+Mawlili@users.noreply.github.com>
2024-09-05 22:34:33 -0500

    Update pfas_analysis_mat_cord.R

--- pfas_analysis_mat_cord.R
+++ pfas_analysis_mat_cord.R
@@ -2,6 +2,7 @@ library(DESeq2)
 library(MASS)
 library(dplyr)
 library(readxl)
+library(apeglm)
 
 load("/rsrch5/home/epi/bhattacharya_lab/users/whwu1/out/salmon_data_concise.RData")
 #filter out NA 
@@ -26,7 +27,7 @@ c_pfas_diff1 <- estimateSizeFactors(c_pfas_diff1)
 idxc1 <- rowSums(counts(c_pfas_diff1, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff1 <- c_pfas_diff1[idxc1,]
 dds_c_pfas1 <- DESeq(c_pfas_diff1)
-res_c_pfas1 <- results(dds_c_pfas1)
+res_c_pfas1 <- lfcShrink(dds_c_pfas1, coef = "cord_PFBA", type = "apeglm")
 
 #PFOA
 c_pfas_diff2 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFOA + cord_PFOA)
@@ -34,7 +35,7 @@ c_pfas_diff2 <- estimateSizeFactors(c_pfas_diff2)
 idxc2 <- rowSums(counts(c_pfas_diff2, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff2 <- c_pfas_diff2[idxc2,]
 dds_c_pfas2 <- DESeq(c_pfas_diff2)
-res_c_pfas2 <- results(dds_c_pfas2)
+res_c_pfas2 <- lfcShrink(dds_c_pfas2, coef = "cord_PFOA", type = "apeglm")
 
 #PFNA
 c_pfas_diff3 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFNA + cord_PFNA)
@@ -42,7 +43,7 @@ c_pfas_diff3 <- estimateSizeFactors(c_pfas_diff3)
 idxc3 <- rowSums(counts(c_pfas_diff3, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff3 <- c_pfas_diff3[idxc3,]
 dds_c_pfas3 <- DESeq(c_pfas_diff3)
-res_c_pfas3 <- results(dds_c_pfas3)
+res_c_pfas3 <- lfcShrink(dds_c_pfas3, coef = "cord_PFNA", type = "apeglm")
 
 #PFDA
 c_pfas_diff4 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFDA + cord_PFDA)
@@ -50,7 +51,7 @@ c_pfas_diff4 <- estimateSizeFactors(c_pfas_diff4)
 idxc4 <- rowSums(counts(c_pfas_diff4, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff4 <- c_pfas_diff4[idxc4,]
 dds_c_pfas4 <- DESeq(c_pfas_diff4)
-res_c_pfas4 <- results(dds_c_pfas4)
+res_c_pfas4 <- lfcShrink(dds_c_pfas4, coef = "cord_PFDA", type = "apeglm")
 
 #PFUnDA
 c_pfas_diff5 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFUnDA + cord_PFUnDA)
@@ -58,7 +59,7 @@ c_pfas_diff5 <- estimateSizeFactors(c_pfas_diff5)
 idxc5 <- rowSums(counts(c_pfas_diff5, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff5 <- c_pfas_diff5[idxc5,]
 dds_c_pfas5 <- DESeq(c_pfas_diff5)
-res_c_pfas5 <- results(dds_c_pfas5)
+res_c_pfas5 <- lfcShrink(dds_c_pfas5, coef = "cord_PFUnDA", type = "apeglm")
 
 #PFBS
 c_pfas_diff6 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFBS + cord_PFBS)
@@ -66,7 +67,7 @@ c_pfas_diff6 <- estimateSizeFactors(c_pfas_diff6)
 idxc6 <- rowSums(counts(c_pfas_diff6, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff6 <- c_pfas_diff6[idxc6,]
 dds_c_pfas6 <- DESeq(c_pfas_diff6)
-res_c_pfas6 <- results(dds_c_pfas6)
+res_c_pfas6 <- lfcShrink(dds_c_pfas6, coef = "cord_PFBS", type = "apeglm")
 
 #PFHxS
 c_pfas_diff7 <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.GA + condition.sex + mat_PFHxS + cord_PFHxS)
@@ -74,7 +75,7 @@ c_pfas_diff7 <- estimateSizeFactors(c_pfas_diff7)
 idxc7 <- rowSums(counts(c_pfas_diff7, normalized=TRUE) >= 5 ) >= 3
 c_pfas_diff7 <- c_pfas_diff7[idxc7,]
 dds_c_pfas7 <- DESeq(c_pfas_diff7)
-res_c_pfas7 <- results(dds_c_pfas7)
+res_c_pfas7 <- lfcShrink(dds_c_pfas7, coef = "cord_PFHxS", type = "apeglm")
 
 
 transcript <- DESeqDataSetFromMatrix(countData = counts_filtered_both, colData = pfas_w1_both, design = ~ W_1 + condition.mother_ethnicity + condition.GROUP + condition.sex + condition.GA)
